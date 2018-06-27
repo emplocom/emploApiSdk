30f711fb95b9a14f779b0ee555af812a99a5324a
Piotr <piotr.pierzgalski@epicode.net>
2018-06-27 15:37:35 +0000

    - removed unnecessary using directives

--- ApiModels/Vacations/IntegratedVacationValidation/VacationValidationRequestModel.cs
+++ ApiModels/Vacations/IntegratedVacationValidation/VacationValidationRequestModel.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EmploApiSDK.ApiModels.Vacations.IntegratedVacationWebhooks.RequestModels;
 
 namespace EmploApiSDK.ApiModels.Vacations.IntegratedVacationValidation
 {
