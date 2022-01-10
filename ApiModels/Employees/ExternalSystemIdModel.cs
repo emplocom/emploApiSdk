c854ea5ed880c587051073b5b6138dd191597fc0
kubw19 <jakub.wlodarczyk@epicode.net>
2022-01-10 16:27:51 +0100

    [#180852513] added model for ExternalSystemIds

---
+++ ApiModels/Employees/ExternalSystemIdModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmploApiSDK.ApiModels.Employees
+{
+    public class ExternalSystemIdModel
+    {
+        public int ExternalSystemId { get; set; }
+        public string ExternalId { get; set; }
+    }
+}
